12af85c6225a3c95c1d66b0cf557b81b5f2873e1
Nickolay Kuznetsov <mudcrab@yandex.ru>
2019-01-07 23:31:34 +0400

    settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,21 @@
 import pygame
 import sys
+from settings import Settings
 
-print("Hello!")
+def run_game():
+    #Инициализация игры,создание экрана
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    #Запуск основного цикла игры
+    while True:
+        #Отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Good Bye!")
+                sys.exit()
+        #Отображение последнего прорисованного экрана
+        screen.fill(ai_settings.bg_color)
+        pygame.display.flip()
+run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения настроек игры"""
+    def __init__(self):
+        """Инициализация настроек игры"""
+        #параметры экрана
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
