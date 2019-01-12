383fe029303d759053b9b121bc78aef7e59108f1
Nickolay Kuznetsov <mudcrab@yandex.ru>
2019-01-12 23:02:34 +0400

    refactor functions

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,16 @@
 import pygame
 import sys
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
 def check_events(ship):
     # Отслеживание событий клавиатуры и мыши
     for event in pygame.event.get():
@@ -8,15 +18,9 @@ def check_events(ship):
             print("Good Bye!")
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_setings, screen, ship):
     screen.fill(ai_setings.bg_color)
