4b78f1a4f435320c5011b61e71d69341af462b18
Nickolay Kuznetsov <mudcrab@yandex.ru>
2019-01-14 00:29:08 +0400

    add bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     #Инициализация игры,создание экрана
@@ -11,11 +12,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
     #Запуск основного цикла игры
     while True:
         #Отслеживание событий клавиатуры и мыши
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets.update()
         #Отображение последнего прорисованного экрана
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,28 +1,40 @@
 import pygame
 import sys
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Создание новой пули и включение ее в группу bullets.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
-def check_events(ship):
+
+
+def check_events(ai_settings, screen, ship, bullets):
     # Отслеживание событий клавиатуры и мыши
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("Good Bye!")
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_setings, screen, ship):
+
+def update_screen(ai_setings, screen, ship, bullets):
     screen.fill(ai_setings.bg_color)
+    # Все пули выводятся позади изображений корабля и пришельцев.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
